3617a4bd0cadab8f0a0eec3f4fef0c9e95c99bd0
Ian Beck <ian@onecrayon.com>
2014-02-07 15:40:14 -0800

    Updated copyright strings for new class (stupid auto-generated junk)

--- Jigger/Classes/OCJiggerColorWell.h
+++ Jigger/Classes/OCJiggerColorWell.h
@@ -3,7 +3,7 @@
 //  Jigger
 //
 //  Created by Ian Beck on 2/7/14.
-//  Copyright (c) 2014 MacRabbit. All rights reserved.
+//  Copyright (c) 2014 One Crayon. MIT license.
 //
 
 #import <Cocoa/Cocoa.h>
